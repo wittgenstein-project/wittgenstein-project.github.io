bddcd14a311c7e2bf01633649270887ac5639b96
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-04-19 20:01:29 +0200

    Fix commit.sh script

--- commit.sh
+++ commit.sh
@@ -3,5 +3,4 @@ set -u
 
 git config --global user.name "${GITHUB_ACTOR}" \
 && git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com" \
-&& git commit -am 'Auto-commit' --author="${COMMIT_AUTHOR}" \
-; git push -u origin HEAD
+&& git commit -am 'Auto-commit'; git push -u origin HEAD
