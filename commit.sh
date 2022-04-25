1d2cbbc2a766d4174f2bfee4c1b6202560efc111
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-04-25 17:33:44 +0200

    Git add new markdown/epub files as part of commit.sh script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -u
 
-git config --global user.name "${GITHUB_ACTOR}" \
-&& git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com" \
-&& git commit -am 'Auto-commit'; git push -u origin HEAD
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add epub/
+git add markdown/
+git commit -am 'Auto-commit'; git push -u origin HEAD
