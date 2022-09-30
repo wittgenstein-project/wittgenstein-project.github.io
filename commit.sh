0da4d1df49ea418d1d9113369867ad6ee8fc57f4
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-09-30 16:31:33 +0100

    Add /mobi folder as part of commit.sh

--- commit.sh
+++ commit.sh
@@ -5,5 +5,6 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add markdown/
 git add epub/
+git add mobi/
 git add pdf/
 git commit -am 'Auto-commit'; git push -u origin HEAD
