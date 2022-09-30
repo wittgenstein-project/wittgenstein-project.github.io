78d7855bf01b2a87f77a6f4f3622d6a32eb8d2ae
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-09-30 15:48:35 +0100

    Generate .mobi files from .md

--- markdown_to_ebook.py
+++ markdown_to_ebook.py
@@ -3,6 +3,7 @@ from shlex import quote
 
 MARKDOWN_DIR = "markdown"
 EPUB_DIR = "epub"
+MOBI_DIR = "mobi"
 
 has_changed_files = subprocess.check_output(
     ["git", "ls-files", "-m", "-o", "--exclude-from=.gitignore"])
@@ -11,18 +12,26 @@ if not has_changed_files:
 else:
     if not os.path.exists(EPUB_DIR):
         os.mkdir(EPUB_DIR)
+    if not os.path.exists(MOBI_DIR):
+        os.mkdir(MOBI_DIR)
     for lang in os.listdir(MARKDOWN_DIR):
-        output_dir = os.path.join(EPUB_DIR, lang)
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
+        epub_output_dir = os.path.join(EPUB_DIR, lang)
+        if not os.path.exists(epub_output_dir):
+            os.mkdir(epub_output_dir)
+        mobi_output_dir = os.path.join(MOBI_DIR, lang)
+        if not os.path.exists(mobi_output_dir):
+            os.mkdir(mobi_output_dir)
         for work in os.listdir(os.path.join(MARKDOWN_DIR, lang)):
-            print(f"*** Converting {work} with pandoc from .md to .epub ***")
-            output_file = os.path.join(output_dir, f"{work}.epub")
+            print(f"*** Converting {work} with pandoc from .md to .epub & .mobi ***")
+            epub_output_file = os.path.join(epub_output_dir, f"{work}.epub")
+            mobi_output_file = os.path.join(mobi_output_dir, f"{work}.mobi")
             input_dir = os.path.join(MARKDOWN_DIR, lang, work)
             input_file = os.path.join(input_dir, f"{work}.md")
             cover_file = os.path.join(input_dir, "cover.png")
-            if os.system(f"pandoc -o {quote(output_file)} {quote(input_file)}"
+            if os.system(f"pandoc -o {quote(epub_output_file)} {quote(input_file)}"
                          " --fail-if-warnings --mathml"
                          f" --epub-cover-image={quote(cover_file)}"
                          f" --resource-path={quote(input_dir)}") != 0:
+                sys.exit(1)
+            if os.system(f"ebook-convert {quote(epub_output_file)} {quote(mobi_output_file)}") != 0:
                 sys.exit(1)
